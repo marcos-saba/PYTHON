b8c1e851dbd2e917b70dccfdc3e128dee5d8cb0b
Marcos Paulo Sabará Fonseca <sabarabm_@hotmail.com>
2024-07-10 15:38:43 -0300

    update

--- PycharmProjects/PythonExercicios/ex0039.py
+++ PycharmProjects/PythonExercicios/ex0039.py
@@ -1,12 +1,24 @@
 from datetime import date
 nascimento = int(input('Informe o ano de nascimento: '))
+sexo = str(input('Informe seu sexo (F/M): ')).strip().upper()
 idade = date.today().year - nascimento
-if idade < 18:
-    print(f'Você precisará se alistar daqui há {18 - idade} anos.')
-elif idade == 18:
-    print('Tá na hora de se alistar!')
+if sexo == 'M':
+    if idade < 18:
+        if 18 - idade == 1:
+            ano = 'ano'
+        else:
+            ano = 'anos'
+        print(f'Você precisará se alistar daqui há {18 - idade} {ano}.')
+        print(f'Seu alistamento será no ano de {nascimento + 18}.')
+    elif idade == 18:
+        print('Tá na hora de se alistar!')
+    else:
+        if idade - 18 == 1:
+            ano = 'ano'
+        else:
+            ano = 'anos'
+        print(f'Já passou da hora de se alistar! '
+              f'Você deveria ter se alistado há {idade - 18} {ano}!'
+              f'\nSeu alistamento foi no ano de {nascimento + 18}.')
 else:
-    print(f'Já passou da hora de se alistar! '
-          f'Você deveria ter se alistado há {idade - 18} anos!')
-
-
+    print('Você é do sexo feminino e não precisa se alistar.')
--- PycharmProjects/PythonExercicios/ex0040.py
+++ PycharmProjects/PythonExercicios/ex0040.py
@@ -1,9 +1,10 @@
 n1 = float(input('Digite a 1ª nota: '))
 n2 = float(input('Digite a 2ª nota: '))
 media = (n1 + n2) / 2
+print('_'*25)
 if media < 5.0:
-    print(f'\nMédia: {media:.1f}\nSituação: \033[31mREPROVADO\033[m')
+    print(f'Média: \033[31m{media:.1f}\033[m\nSituação: \033[31mREPROVADO\033[m')
 elif 5.0 <= media <= 6.9:
-    print(f'Média: {media}\nSituação: \033[33mRECUPERAÇÃO\033[m')
+    print(f'Média: \033[33m{media:.1f}\033[m\nSituação: \033[33mRECUPERAÇÃO\033[m')
 else:
-    print(f'Média: {media}\nSituação: \033[34mAPROVADO\033[m')
+    print(f'Média: \033[34m{media:.1f}\033[m\nSituação: \033[34mAPROVADO\033[m')
