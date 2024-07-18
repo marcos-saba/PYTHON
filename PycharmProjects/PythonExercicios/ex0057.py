84a71d5b733ef9dc1dad7cfcfa830ffebc522b6b
Marcos Paulo Sabará Fonseca <sabarabm_@hotmail.com>
2024-07-18 17:41:46 -0300

    update

--- PycharmProjects/PythonExercicios/ex0057.py
+++ PycharmProjects/PythonExercicios/ex0057.py
@@ -1,6 +1,9 @@
-sexo = ''
-while sexo not in ('M', 'F'):
-    sexo = str(input('Digite o sexo [M/F]: ')).upper().strip()
-    if sexo not in ('M', 'F'):
-        print('Valor \033[31minválido\033[m. Tente novamente.')
+sexo = str(input('Informe seu sexo [M/F]: ')).upper().strip()[0]
+while sexo not in 'MF':
+    sexo = str(input('Valor \033[31mINVÁLIDO!\033[m! Informe seu sexo [M/F]: ')).upper().strip()[0]
+print('-'*40)
+if sexo == 'M':
+    print('Sexo masculino registrado com sucesso!')
+else:
+    print('Sexo feminino registrado com sucesso!')
 print('Fim!')
