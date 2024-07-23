b560bc212506b154942562745eaad7842332a6af
Marcos Paulo Sabará Fonseca <sabarabm_@hotmail.com>
2024-07-23 14:14:27 -0300

    update

--- PycharmProjects/CursoemVideo/aula14.py
+++ PycharmProjects/CursoemVideo/aula14.py
@@ -1,18 +1,20 @@
 '''for c in range(1, 10):
     print(c)
-print('FIM')'''
-
-'''c = 1
+print('FIM')
+--------------------------------------------------------
+c = 1
 while c < 10:
     print(c)
     c += 1
-print('FIM')'''
-
-'''r = 'S'
+print('FIM')
+--------------------------------------------------------
+r = 'S'
 while r == 'S':
     n = int(input('Digite um valor: '))
     r = str(input('Quer continuar? [S/N] ')).upper()
-print('Fim')'''
+print('Fim')
+--------------------------------------------------------
+'''
 
 n = 1
 par = impar = 0
