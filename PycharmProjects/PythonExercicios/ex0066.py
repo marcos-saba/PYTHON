7acb4275cbdcf17ebd8b81da4e27cda86b557a1f
Marcos Paulo Sabará Fonseca <sabarabm_@hotmail.com>
2024-07-23 18:41:29 -0300

    update

---
+++ PycharmProjects/PythonExercicios/ex0066.py
@@ -0,0 +1,9 @@
+soma = 0
+cont = 0
+while True:
+    num = int(input('Digite um número (999 para parar): '))
+    if num == 999:
+        break
+    soma = soma + num
+    cont = cont + 1
+print(f'A soma dos {cont} números vale {soma}.')
---
+++ PycharmProjects/PythonExercicios/ex0069.py
@@ -0,0 +1,14 @@
+print(f'{' TABUADA 2.1 ':=^50}\n')
+while True:
+    n = int(input('Digite um número para mostrar sua tabuada: '))
+    if n < 0:
+        break
+    cont = 1
+    print('-'*50)
+    while cont <= 10:
+        r = n*cont
+        print(f'{n:>20} x {cont:>2} = {r}')
+        cont += 1
+    print('-'*50)
+print('-'*50)
+print(f'{'Programa encerrado.':^50}')
