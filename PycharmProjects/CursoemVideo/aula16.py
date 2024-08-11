3a0a72292dadd822863e01031a824a4ee3e9830e
Marcos Paulo Sabará Fonseca <sabarabm_@hotmail.com>
2024-08-11 11:51:52 -0300

    update

---
+++ PycharmProjects/CursoemVideo/aula16.py
@@ -0,0 +1,17 @@
+'''lanche = ('hambúrguer', 'suco', 'pizza', 'pudim')
+for c in lanche:
+    print(f'Eu vou comer {c}!')
+print('-'*20)
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]}!')
+print('-'*30)
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida}, na posição {pos}')
+print(sorted(lanche))
+'''
+#del(pessoa)
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b
+print(c.count(5))
+print(c.index(8, 1))
